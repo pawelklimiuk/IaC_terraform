d38bc297c98aa2704fae087cfd47ff88247e23b6
pawel <33744262+pawelklimiuk@users.noreply.github.com>
2024-11-22 18:13:41 +0100

    inicializacja2

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
 provider "aws" {
     region = "us-east-1"  
 }
+terraform {
+  backend "s3" {
+    bucket = "pawel-klimiuk-panda-devops-core-19"
+    key = "infra/terraform.state"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "3.33"
+    }
+  }
+  required_version = ">=1.0.0"
+}
