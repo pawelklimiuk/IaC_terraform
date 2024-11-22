8818ebe1f7a63ee9a5f918622a3bd403f4474e4a
Pawel Klimiuk <33744262+pawelklimiuk@users.noreply.github.com>
2024-11-22 17:38:37 +0000

    ansible2

--- ansible.tf
+++ ansible.tf
@@ -7,3 +7,9 @@ resource "null_resource" "download_ssh_key" {
     command = "mkdir -p ~/.ssh/ &&  aws s3 cp s3://${var.bucket_name}/panda_kurs.pem ~/.ssh/panda_kurs.pem || true && chmod 400 ~/.ssh/panda_kurs.pem"
   }
 }
+
+resource "local_file" "ansible_inventory" {
+  content = templatefile("inventory.tpl", 
+    { ansible_ip = "${join("\n", aws_instance.panda.*.public_ip)}" })
+  filename = "${path.module}/../inventory"
+}
